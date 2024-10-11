37b04f9aeb30c70816c95dfdc11b8567851f48bb
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2024-10-11 18:14:35 +0000

    react App course

---
+++ React-Course/my-blog/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> My Awesome Blob</h1>
+     <div id="page-body">
+      Welcome to my Blog!
+      </div>
+    </div>
+  );
+}
+
+export default App;

ebdc3bb2d1bafc67cce1082631ed4e0c8cd5237a
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2024-10-12 16:47:04 +0000

    im missing a installation of mongodb

--- React-Course/my-blog/src/my-blog-backend/src/server.js
+++ React-Course/my-blog/src/my-blog-backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 let articleInfo = [{
     name : 'learn-react',
@@ -11,12 +12,45 @@ let articleInfo = [{
     upvotes:0,
 }]
 
+
+
 const app = express();
 app.use(express.json());
 
-app.put('/api/articles/:name/upvote',(req,res)=>{
-
+aa.get('/api/articles/:name',(req,res)=>{
+    const {name}= req.params;
+const client = new MongoClient('mongodb://127.0.0.1:27017')
 })
+
+
+
+app.put('/api/articles/:name/upvote',(req,res)=>{
+const {name}=req.params;
+const article = articleInfo.find(a=>a.name === name);
+if (article) {
+    article.upvotes +=1;
+    res.send(`the ${name} article now has ${article.upvotes} upvotes!!`);
+    
+}else{
+    res.send(`That article doesn\`t existt`);
+}
+});
+
+app.post('/api/articles/:name/comments',(req,res)=>{
+    const {name}= req.params;
+    const {postedBy,text} = req.body;
+    const article = articleInfo.find(a => a.name ===name);
+
+if (article) {
+    res.send(article.comments);
+    article.comments.push(postedBy.text);
+} else {
+    res.send(`That article doesn\`t existt`);
+
+}
+})
+
+
 
 app.listen(8000,()=>{
     console.log("Server is lising on port 8000")
